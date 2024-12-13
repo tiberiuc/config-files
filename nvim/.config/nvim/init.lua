77f69439f9101fa5027c4e4ff2f8b5546f14226a
Tibi Craciun <tibi@happysoft.ro>
2024-12-13 12:06:27 +0200

    refactor telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,9 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 require("config.lazy")
 
 vim.opt.relativenumber = true
@@ -6,11 +12,14 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.swapfile = false
+vim.opt.breakindent = true
 
 
 vim.keymap.set("n", "<space>w", ":w<CR>")
 vim.keymap.set("n", "<space>q", ":q<CR>")
 vim.keymap.set("n", "<space>h", ":noh<CR>")
+vim.keymap.set("n", "<space>v", ":vsplit<CR>")
+vim.keymap.set("n", "<space>Q", ":q!<CR>")
 vim.keymap.set("n", "<space><space>", "<C-6>")
 
 vim.opt.clipboard = "unnamedplus"
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,24 +12,33 @@ return {
         -- For major updates, this must be adjusted manually.
         version = "^1.0.0",
       },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
     },
     config = function()
       require('telescope').setup({
         extensions = {
-          fzf = {}
+          fzf = {
+            fuzzy = true,
+            case_mode = "smart_case"
+          },
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
         }
       })
       require("telescope").load_extension("fzf")
       require("telescope").load_extension("recent_files")
+      require("telescope").load_extension("ui-select")
       local builtin = require('telescope.builtin')
       local ivy = require('telescope.themes').get_ivy()
-      vim.keymap.set('n', '<leader>ff', function() builtin.find_files(ivy) end, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', function() builtin.live_grep(ivy) end, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>fF', function() require("telescope").extensions.live_grep_args.live_grep_args(ivy) end,
+      vim.keymap.set('n', '<leader>sf', function() builtin.find_files(ivy) end, { desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>st', function() builtin.live_grep(ivy) end, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>sl', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sa', function() require("telescope").extensions.live_grep_args.live_grep_args(ivy) end,
         { desc = "Advance searc ex: \"Repo\" --iglob *.exs " })
-      vim.api.nvim_set_keymap("n", "<Leader>fr",
+      vim.api.nvim_set_keymap("n", "<Leader>sr",
         [[<cmd>lua require('telescope').extensions.recent_files.pick()<CR>]],
         { noremap = true, silent = true, desc = "Telescope recent files" })
     end
