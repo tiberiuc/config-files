12eed66b625259fd50e372ffefdb3103226d6702
Tibi Craciun <tibi@happysoft.ro>
2024-12-16 14:22:05 +0200

    bump elixir ls version

--- nvim/.config/nvim/lua/plugins/elixir.lua
+++ nvim/.config/nvim/lua/plugins/elixir.lua
@@ -20,7 +20,7 @@ return {
           enable = false
         },
         elixirls = {
-          tag = "v0.24.1",
+          tag = "v0.25.0",
           enable = true,
           settings = elixirls.settings {
             dialyzerEnabled = true,
