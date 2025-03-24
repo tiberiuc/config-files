fedea23e496888eb5652358cb0afb58a8c212973
Tibi Craciun <tibi@happysoft.ro>
2025-03-25 00:33:26 +0200

    Fixed mini.files too open current file folder

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -7,7 +7,10 @@ return {
       }
       require('mini.git').setup({})
       require('mini.icons').setup({})
-      vim.keymap.set('n', '<space>e', function() require('mini.files').open() end)
+      local mini_files = require('mini.files')
+      vim.keymap.set('n', '<space>e', function() mini_files.open(mini_files.get_latest_path()) end)
+      -- vim.keymap.set('n', '<space>e', function() mini_files.open() end)
+      vim.keymap.set('n', '<space>E', function() mini_files.open(vim.api.nvim_buf_get_name(0)) end)
     end
   }
 }
